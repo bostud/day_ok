c582279d317126504a54296bd24a1be2f2bb3824
b.studenets <b.studenets@smartweb.com.ua>
2021-02-11 20:07:43 +0200

    allowed hosts

--- day_ok/day_ok/urls.py
+++ day_ok/day_ok/urls.py
@@ -16,10 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from ..schedule.views import home_view
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_view, name='home'),
 ]
