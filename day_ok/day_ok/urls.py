22234fe13ddce2446af42f02f2b9a2a63fb57979
b.studenets <b.studenets@smartweb.com.ua>
2021-02-11 20:07:00 +0200

    allowed hosts

--- day_ok/day_ok/urls.py
+++ day_ok/day_ok/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from day_ok.schedule.views import home_view
+from ..schedule.views import home_view
 
 
 urlpatterns = [
