bc541a8436570bd7d43599d93b537651e6df2dfa
b.studenets <b.studenets@smartweb.com.ua>
2021-02-28 04:27:41 +0200

    some stuff

--- day_ok/schedule/models.py
+++ day_ok/schedule/models.py
@@ -92,7 +92,7 @@ class ContactMixin(models.Model):
 
     @property
     def full_name(self):
-        return f"{self.first_name} {self.last_name}"
+        return f"{self.last_name} {self.first_name}"
 
 
 class Teacher(ContactMixin):
