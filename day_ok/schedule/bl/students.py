cd0a77df0429f44dac316dc5e451ba47b5851b70
b.studenets <b.studenets@smartweb.com.ua>
2021-03-11 13:35:59 +0200

    add some functional

--- day_ok/schedule/bl/students.py
+++ day_ok/schedule/bl/students.py
@@ -1,5 +1,5 @@
 from typing import List, Optional
-from ..models import Student, Lessons
+from ..models import Student, Lessons, Group, Invoice
 from ..utils import datetime_now_tz as now
 
 from django.db.models import Q
@@ -13,13 +13,39 @@ def get_student(id_: int) -> Optional[Student]:
     return Student.objects.filter(id=id_).first()
 
 
-def get_student_lessons(
+def get_student_future_lessons(
     student_id: int,
     count: int = 5,
 ) -> List[Optional[Lessons]]:
     dt_now = now()
-    return Lessons.objects.filter(
+    lessons = Lessons.objects.filter(
         Q(student_id=student_id) | Q(group__students__in=[student_id]),
         date__gte=dt_now.date(),
-        time_end__gte=dt_now.time(),
     ).order_by('time_end').order_by('date')[:count]
+    return list(filter(lambda l: not l.is_finished, lessons))
+
+
+def get_student_all_lessons(
+    student_id: int,
+) -> List[Optional[Lessons]]:
+    return Lessons.objects.filter(
+        Q(student_id=student_id) | Q(group__students__in=[student_id]),
+    ).order_by('time_start').order_by('-date')
+
+
+def get_student_groups(
+    student_id: int,
+) -> List[Optional[Group]]:
+    return Group.objects.filter(
+        students__in=[Student.objects.filter(id=student_id).first()]
+    )
+
+
+def get_student_invoices(
+    student_id: int,
+) -> List[Optional[Invoice]]:
+    return Invoice.objects.filter(
+        student_id=student_id
+    ).order_by(
+        '-date_created'
+    )
--- day_ok/schedule/data_classes/teachers.py
+++ day_ok/schedule/data_classes/teachers.py
@@ -1,6 +1,6 @@
 from typing import List
 from dataclasses import dataclass
-from ..models import Lessons, Subject, INDIVIDUAL
+from ..models import Lessons, Subject
 
 
 @dataclass
@@ -13,10 +13,10 @@ class SubjectReport:
 @dataclass
 class LessonsReport:
     lessons_type_name: str
-    lessons_type_id: str
+    lessons_type_id: int
     count: int
     subjects_reports: List[SubjectReport]
 
     @property
     def is_individual(self):
-        return self.lessons_type_id == INDIVIDUAL
+        return int(self.lessons_type_id) == Lessons.Type.INDIVIDUAL
